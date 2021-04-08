b1958148666da4ef0c736e6f40d61c093236d9df
Bruno <brunobrandao1523@gmail.com>
2021-04-08 19:29:08 -0300

    debug prints

--- projects/morel/run_morel.py
+++ projects/morel/run_morel.py
@@ -629,6 +629,8 @@ for outer_iter in range(job_data["num_iter"]):
     f1 = (2 * precision * recall) / (precision + recall)
 
     # Confusion Matrix
+    print(type(true_actions))
+    print(type(pred_actions))
     cm = confusion_matrix(true_actions, pred_actions.round())
 
     figure = utils.plot_confusion_matrix(cm, class_names=["Don't activate", "Activate"])
